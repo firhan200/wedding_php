abe1f5a0177b91772731145d3b1852061ad62cb7
firhan faisal <firhan@jmdigital.id>
2022-05-23 17:44:02 +0700

    update

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nadia &amp; Firhan</title>
+    <meta name="description" content="Pernikahan Nadia dan Firhan">
+    <meta name="author" content="Firhan">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nadia dan Firhan</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="<?php echo site_url('/assets/css/style.css') ?>" rel="stylesheet"/>
