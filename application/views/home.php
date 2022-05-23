e33b55732105bcb8cbe84931e3a8d76a3aae795e
firhan faisal <firhan@jmdigital.id>
2022-05-23 18:48:36 +0700

    open invitation

--- application/views/home.php
+++ application/views/home.php
@@ -1,20 +1,4 @@
 <!-- firework -->
-<div class="firework-1"></div>
-<div class="firework-2"></div>
-<div class="firework-3"></div>
-<div class="firework-4"></div>
-<div class="firework-5"></div>
-<div class="firework-6"></div>
-<div class="firework-7"></div>
-<div class="firework-8"></div>
-<div class="firework-9"></div>
-<div class="firework-10"></div>
-<div class="firework-11"></div>
-<div class="firework-12"></div>
-<div class="firework-13"></div>
-<div class="firework-14"></div>
-<div class="firework-15"></div>
-<!-- firework -->
 
 <div class="first-show banner-image" id="first-card">
     <div class="container-fluid h-100">
